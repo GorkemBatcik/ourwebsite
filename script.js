e8fbd80df8af50f230758ecdbe2cced4740bb3b6
Görkem Batcık <gorkembatcik@Gorkem-MacBook-Air.local>
2025-07-16 17:42:39 +0300

    sayfa temeli atıldı

---
+++ script.js
@@ -0,0 +1,28 @@
+function tarihFarki(baslangic) {
+  const simdi = new Date();
+  let yillar = simdi.getFullYear() - baslangic.getFullYear();
+  let aylar = simdi.getMonth() - baslangic.getMonth();
+  let gunler = simdi.getDate() - baslangic.getDate();
+
+  if (gunler < 0) {
+    aylar -= 1;
+    gunler += new Date(simdi.getFullYear(), simdi.getMonth(), 0).getDate();
+  }
+
+  if (aylar < 0) {
+    yillar -= 1;
+    aylar += 12;
+  }
+
+  return { yillar, aylar, gunler };
+}
+
+function sayaçGuncelle() {
+  const baslangicTarihi = new Date("2024-10-22T18:00:00");
+  const fark = tarihFarki(baslangicTarihi);
+
+  const metin = `${fark.yillar} yıl, ${fark.aylar} ay, ${fark.gunler} gün geçti`;
+  document.getElementById("sayac").textContent = metin;
+}
+
+sayaçGuncelle();
